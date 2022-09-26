4dbbd030c2061c741e40fc4ba5d64e4c0e13f01d
Yeongmin_Chae <codudals98@naver.com>
2022-09-26 18:42:09 +0900

    portfolio day 4 : best horror scene part 1 ( break for a while )

--- pages/CSS_frontEnd/Best_Horror_Scene/components/MovieInfo.tsx
+++ pages/CSS_frontEnd/Best_Horror_Scene/components/MovieInfo.tsx
@@ -1,11 +1,9 @@
 import Image, { StaticImageData } from "next/image";
-import Misery from "../image/misery.png";
 
 interface ImovieProps {
   title: string;
   director: string;
   rating: number;
-  // Images: "Misery" | "Shining" | "Lamb";
   Images: StaticImageData;
   [key: string]: any; // this is allowed to us to send any other props to my input
 }
@@ -18,88 +16,80 @@ export default function MovieInfo({
   ...rest
 }: ImovieProps) {
   return (
-    <div className="w[500px] h-[600px] bg-[#e7473c] ">
-      <div className="pt-16 ">
-        <div className="flex items-start justify-start">
-          <div className="ml-[1.8vh] mt-0 w-[500px] text-4xl font-bold text-white hover:first-letter:visible">
-            <span className="invisible ml-[0.8vh] text-4xl text-slate-800 opacity-60">
-              #
-            </span>{" "}
-            {title}
-          </div>
+    <div className="h-[700px] bg-[#e7473c] sm:h-[750px] ">
+      <div className="pt-16 sm:pt-12 ">
+        <div className=" mt-0 ml-[1.8vh] text-4xl font-bold text-white hover:first-letter:visible sm:ml-[5vh] sm:text-[44px] ">
+          <span className="invisible text-slate-800 opacity-60 ">#</span>{" "}
+          {title}
         </div>
-        <h3 className="ml-[7vh] mt-2 text-sm text-white">{director}</h3>
+        <h3 className="mt-2 ml-[7vh] text-sm text-white sm:ml-[10vh] ">
+          {" "}
+          {director}
+        </h3>
       </div>
-      <div className="grid ">
-        <div className="ml-[7vh] mt-5 flex ">
-          <div className="relative px-[200px] pb-[225px]">
-            {
-              <Image
-                src={Images}
-                alt={title + "Posture"}
-                layout="fill"
-                placeholder="blur"
-                className="object-contain "
+      <div className="grid">
+        <div className="mt-5 flex items-start justify-start sm:ml-[10vh]">
+          <div className="relative ml-[7vh] px-48 pb-56 sm:-ml-[0.7vh] sm:px-72 sm:pb-80 md:px-80 md:pb-96 ">
+            <Image
+              src={Images}
+              alt={title + "Posture"}
+              layout="fill"
+              placeholder="blur"
+              className=" object-contain"
+            />
+          </div>
+          <div className="mt-1 flex h-24 w-12 flex-col items-center justify-around bg-red-600 text-white shadow-md sm:-ml-[1vh] sm:mt-[1.3px] ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
               />
-            }{" "}
-          </div>
-          <div className="flex h-[100px] w-[9.5vw] flex-col items-center justify-around">
-            <div className="flex h-[33%] w-[9vw] items-center justify-center bg-red-600 text-white shadow-md hover:bg-red-700 hover:shadow-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
-                />
-              </svg>
-            </div>
-            <div className="flex h-[33.33%] w-[9vw] items-center justify-center bg-red-600 text-white shadow-md hover:bg-red-700 hover:shadow-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
-                />
-              </svg>
-            </div>
-            <div className="flex h-[33.33%] w-[9.5vw] items-center justify-center bg-red-600 text-white shadow-md hover:bg-red-700 hover:shadow-lg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
-                />
-              </svg>
-            </div>
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+              />
+            </svg>
           </div>
         </div>
       </div>
-      <span className="mr-[7vh] mt-9 flex justify-end text-[0.9rem] tracking-[0.1em] text-black text-opacity-60">
+      <span className="mr-[7vh] mt-9 flex justify-end text-[0.9rem] tracking-[0.1em] text-black text-opacity-60 sm:mr-[10vh] sm:mt-11">
         My Rating: {rating}/10
       </span>
       <div className="mx-[7vh] mt-16 flex justify-between text-[0.9rem] tracking-[0.1em] text-black text-opacity-60">
-        <span className="mt-[0.8] flex items-center justify-around">
+        <span className="mt-[0.8] flex items-center justify-around sm:ml-5">
           No Comments
         </span>
         <div className="flex items-center justify-around">
@@ -119,10 +109,12 @@ export default function MovieInfo({
               />
             </svg>
           </span>
-          <span className="ml-1">Leave a Comment</span>
+          <span className="ml-1 underline underline-offset-4 opacity-20 sm:mr-[3vh] ">
+            Leave a Comment
+          </span>
         </div>
       </div>
-      <div className="h-[600px] w-[500px] bg-gradient-to-b from-[#e7473c] to-red-700 shadow-md" />
+      <div className="h-[80px] bg-gradient-to-b from-[#e7473c] to-red-600 sm:h-[80px]" />
     </div>
   );
 }
--- pages/CSS_frontEnd/Best_Horror_Scene/index.tsx
+++ pages/CSS_frontEnd/Best_Horror_Scene/index.tsx
@@ -9,9 +9,9 @@ import MovieInfo from "./components/MovieInfo";
 
 const Best_Horror_Scene: NextPage = () => {
   return (
-    <div className="grid h-full max-w-3xl overflow-y-visible md:max-w-5xl lg:max-w-7xl xl:max-w-full">
-      <div className="left-0 top-0 h-[90vh] flex-col justify-between bg-[#F0F0F0] pt-8 pl-[5vh] pr-0 md:max-w-5xl md:pl-[9vh] lg:max-w-7xl xl:max-w-full">
-        <h1 className="mt-8 pt-[14px] font-poppins text-5xl leading-[50px] tracking-[0.3em] text-[#e7473c] md:text-7xl lg:text-9xl  ">
+    <div className="grid max-w-3xl overflow-y-visible sm:h-screen sm:max-w-5xl md:col-span-2 md:max-w-6xl md:grid-cols-2 md:overflow-x-hidden lg:max-w-7xl xl:max-w-full ">
+      <div className="top-0 h-[90vh] bg-[#F0F0F0] pt-8 pl-[5vh] pr-0 sm:h-[101vh] sm:pl-[9vh] md:overflow-y-visible md:pl-[11vh]">
+        <h1 className="mt-8 pt-[14px] font-poppins text-5xl leading-[50px] tracking-[0.3em] text-[#e7473c] sm:text-7xl md:text-5xl ">
           BEST
           <div className="flex">
             <span className=" " />H<span className="">O</span>
@@ -44,19 +44,19 @@ const Best_Horror_Scene: NextPage = () => {
           <span className="flex" />
           SCENES
         </h1>
-        <div className="relative block h-[293.234px] text-justify tracking-wide  ">
-          <h3 className="leading-11 mt-10 block h-[57.5838px] w-[400px] text-2xl text-[#e7473c]">
+        <div className="relative block h-[293.234px] w-[400px] text-justify sm:w-[500px] md:w-[400px] ">
+          <h3 className="mt-10 block h-[57.5838px] text-2xl leading-10 text-[#e7473c] sm:mt-14 md:h-[62px] md:w-[220px] md:text-xl md:tracking-tighter">
             An ever growing collection featuring some of the best scenes in
             horror.
           </h3>
-          <p className="text-l mt-8 block h-[57.5838px] w-[400px] text-justify leading-5 tracking-wide text-[#e7473c]  ">
+          <p className="mt-8 block h-[57.5838px] text-justify leading-5 tracking-wide text-[#e7473c]  ">
             “Best Horror Scenes” is a collection of scenes I feel are some of
             the most affecting in horror. Some may be simple black cat scares,
             others may be more subdued or nuanced. Many come from films that
             aren&apos;t necessarily “horror” but have elements or threads of
             horror.
           </p>
-          <nav className=" mt-24 h-[100px] w-[380px] text-sm ">
+          <nav className=" mt-24 h-[100px] text-sm sm:mt-20 ">
             <div className="flex flex-wrap ">
               <Link href="">
                 <span className="mr-[12px] mb-2 cursor-pointer leading-3 underline underline-offset-2 opacity-60 hover:opacity-90">
@@ -94,7 +94,7 @@ const Best_Horror_Scene: NextPage = () => {
                 </span>
               </Link>
             </div>
-            <p className="mt-4 flex w-[400px] items-center justify-center pb-10 text-[3px] leading-3 tracking-wide text-black text-opacity-[0.35]">
+            <p className="mt-4 flex items-center justify-center pb-10 text-[3px] leading-3 tracking-wide text-black text-opacity-[0.35] sm:mt-6">
               Best Horror Scenes is a love letter to the Horror film genre by
               Brandon Durham. Most film clips contain major spoilers, and it is
               recommended you don’t watch the clip if you plan on seeing the
@@ -103,9 +103,9 @@ const Best_Horror_Scene: NextPage = () => {
           </nav>
         </div>
       </div>
-      <div className="h-[90vh] w-[500px]">
-        <div className="sticky top-0 z-10 block h-[120px] w-[500px] overflow-hidden bg-[#e7473c] tracking-[0.1em] outline-dashed outline-[0.1px]">
-          <span className="block py-[32px] px-[40px] text-center text-base font-light text-white ">
+      <div className="h-[90vh]">
+        <div className="sticky top-0 z-10 h-[120px] overflow-hidden bg-[#e7473c] tracking-[0.1em] outline-dashed outline-[0.1px] sm:h-[105px]">
+          <div className="block px-12 py-8 text-center text-base font-light text-white ">
             Currently viewing{" "}
             <span className="text-lg font-light text-black underline underline-offset-4 opacity-80 hover:text-white ">
               everything
@@ -115,26 +115,28 @@ const Best_Horror_Scene: NextPage = () => {
               random
             </span>{" "}
             .
-          </span>
+          </div>
         </div>
-        <MovieInfo
-          title="Misery (1990)"
-          director="Directed by Rob Reiner"
-          rating={9}
-          Images={Misery}
-        />
-        <MovieInfo
-          title="The Shining (1980)"
-          director=" Directed by Stanley Kubrick"
-          rating={10}
-          Images={Shining}
-        />
-        <MovieInfo
-          title=" The Silence of the Lambs (1991)"
-          director=" Directed by Jonathan Demme"
-          rating={9}
-          Images={Lamb}
-        />
+        <div>
+          <MovieInfo
+            title="Misery (1990)"
+            director="Directed by Rob Reiner"
+            rating={9}
+            Images={Misery}
+          />
+          <MovieInfo
+            title="The Shining (1980)"
+            director=" Directed by Stanley Kubrick"
+            rating={10}
+            Images={Shining}
+          />
+          <MovieInfo
+            title=" The Silence of the Lambs (1991)"
+            director=" Directed by Jonathan Demme"
+            rating={9}
+            Images={Lamb}
+          />
+        </div>
       </div>
     </div>
   );
