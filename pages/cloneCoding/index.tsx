c3eb543e4d62059c07eff079dd75bccc399c7b26
Yeongmin_Chae <codudals98@naver.com>
2022-09-29 18:48:34 +0900

    portfolio day 6 : clone coding Main Page

--- pages/cloneCoding/index.tsx
+++ pages/cloneCoding/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from "next";
-import { motion } from "framer-motion";
-import Link from "next/link";
+import Moment, { now } from "moment";
 
 const Home: NextPage = () => {
+  const todayIs = Moment().format("MM월 DD일");
+  const dateIs = Moment().day();
+  const dateList = ["월", "화", "수", "목", "금", "토", "일"];
   return (
-    <div>
-      <div>first work. this is smth</div>
+    <div className="">
+      <span className="mx-3 mt-5 flex text-xs opacity-50 ">
+        {todayIs} {dateList[dateIs]}요일
+      </span>
+      <div className="mx-5">
+        <div className="mt-1 flex items-center justify-between">
+          <div className="text-3xl font-semibold">PORTFOLIO</div>
+          <div className="h-8 w-8 rounded-full bg-red-300 " />
+        </div>
+        <div className="mt-3 h-96 w-full rounded-xl bg-indigo-300 shadow-2xl ">
+          <div>first work. this is smth</div>
+        </div>
+      </div>
     </div>
   );
 };
