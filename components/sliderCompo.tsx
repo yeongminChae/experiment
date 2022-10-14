5c38e52a5ee35a08966360f416f84633a4d7d467
Yeongmin_Chae <codudals98@naver.com>
2022-10-14 22:44:01 +0900

    portfolio day 11 part 2

---
+++ components/sliderCompo.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from "next";
+import { motion } from "framer-motion";
+
+interface ISlider {
+  xInit: number | string;
+  xEnd: number | string;
+}
+export default function SliderCompo({ xInit, xEnd }: ISlider) {
+  return (
+    <>
+      <motion.div className="relative mx-6 flex">
+        {[...Array(10)].map((i, _) => (
+          <motion.div
+            key={i}
+            initial={{ x: xInit, opacity: 1 }}
+            animate={{ x: xEnd }}
+            transition={{
+              duration: 30,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            exit={{ opacity: 0 }}
+            className="m-2 mb-3 mt-7 flex h-[4.5rem] w-[4.5rem] rounded-lg bg-red-200"
+          >
+            {i}{" "}
+          </motion.div>
+        ))}
+      </motion.div>
+    </>
+  );
+}
