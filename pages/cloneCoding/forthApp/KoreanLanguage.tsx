939df0d64ab2bacf952057322abccde16c9e5974
Yeongmin_Chae <codudals98@naver.com>
2022-11-20 19:56:40 +0900

    portfolio day 36 part 1

--- pages/cloneCoding/forthApp/KoreanLanguage.tsx
+++ pages/cloneCoding/forthApp/KoreanLanguage.tsx
@@ -1,13 +1,37 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import AppModalNaming from "../../../components/appModalNaming";
 import { IModal } from "../../../components/appModals";
 import AppModalTopFirstPart from "../../../components/appModalTopFirstPart";
+import { cls } from "../../../libs/client/utils";
 
 export default function KoreanLanguage({ title }: IModal) {
   const router = useRouter();
   const currentLoca = router.asPath.split("/");
+  const [description, setDescription] = useState("");
+  const [more, setMore] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setDescription(
+      "KoreanLanguage 앱 화면에 떠오르는 동물의 이미지 에니메이션을 보고 알맞은 카드를 선택하세요! 귀여운 동물의 이미지와 눈을 즐겁게 만들어주는 에니메이션을 보고 계신다면 시간이 어떻게 갔는지 모를 수 있습니다 !! \n" +
+        "\n이 application은 TypeScript , NextJs(React) 를 활용하여 제작한 Front-End application입니다. 또한 시각적 효과를 위하여  Framer Motion 을 사용했으며 이 application을 제작하는데 중요한 아이디어를 제공해준 slider effect을 비롯하여  다양한 animation 효과 또한 구현했습니다.\r" +
+        "\n 이 application을 제작하면서 특히 재미있었던 점은 마지막 페이지에 첨부한 동물 card animation 입니다. JavaScript에 기본적인 event listener 효과인 mouseEnter mouseLeave 를 활용한 Hovering animation 구현이 기억에 남습니다.\r" +
+        "구현하는데 힘들었던 부분은 랜덤으로 제공되어지는 카드를 구현하는 것이 힘들었습니다. 그러나 이 과정을 통하여 NextJs에서에 SSR 데이터 처리에 대하여 공부할 수 있는 기회가 되었습니다. "
+    );
+  }, [description, setDescription]);
+  const onMoreClick = () => {
+    setMore(true);
+  };
+  useEffect(() => {
+    if (more) {
+      onMoreClick();
+    }
+  }, [more]);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   const toggleLeaving = () => {
     router.push("/cloneCoding", undefined, { scroll: false });
   };
@@ -15,61 +39,116 @@ export default function KoreanLanguage({ title }: IModal) {
     router.push("/").then(() => window.scrollTo(2100, 2100));
   };
   return (
-    <AnimatePresence>
-      {router.asPath === `/cloneCoding/forthApp/KoreanLanguage` && (
-        <motion.div layoutId={currentLoca[3]} className="absolute z-10">
-          <ContextPart className="fixed top-0 h-full w-full bg-[#F9F9F9] shadow-xl sm:ml-20 sm:w-[35rem] md:ml-40 md:w-[40rem] lg:ml-48 xl:ml-[23rem] ">
-            <div className="mb-16 ml-3">
-              <button
-                onClick={toggleLeaving}
-                className="absolute top-5 ml-3 flex h-7 w-7 items-center justify-center rounded-full bg-white/80 shadow-md"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="black"
-                  strokeOpacity="0.3"
-                  className="mr-0.5 h-5 w-5"
+    mounted && (
+      <AnimatePresence>
+        {router.asPath === `/cloneCoding/forthApp/KoreanLanguage` && (
+          <motion.div
+            layoutId={currentLoca[3]}
+            className="absolute z-10 xl:ml-10 "
+          >
+            <ContextPart className="fixed top-0 h-full w-[50rem] bg-[#F9F9F9] shadow-xl sm:ml-20 sm:w-[35rem] md:ml-40 md:w-[40rem] lg:ml-48 xl:ml-[23rem] ">
+              <div className="mb-16 ml-3">
+                <button
+                  onClick={toggleLeaving}
+                  className="absolute top-5 ml-3 flex h-7 w-7 items-center justify-center rounded-full bg-white/80 shadow-md"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 19.5L8.25 12l7.5-7.5"
-                  />
-                </svg>
-              </button>
-              <button
-                onClick={toggleBefore}
-                className="absolute top-5 ml-14 flex h-7 w-7 items-center justify-center rounded-full bg-white/80 shadow-md"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1}
-                  strokeOpacity="0.5"
-                  fill="#7a8494"
-                  stroke="black"
-                  className="h-5 w-5"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="black"
+                    strokeOpacity="0.3"
+                    className="mr-0.5 h-5 w-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 19.5L8.25 12l7.5-7.5"
+                    />
+                  </svg>
+                </button>
+                <button
+                  onClick={toggleBefore}
+                  className="absolute top-5 ml-14 flex h-7 w-7 items-center justify-center rounded-full bg-white/80 shadow-md"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                  />
-                </svg>
-              </button>
-            </div>
-            <AppModalNaming title={title} />
-            <div className="mt-5 ml-6 border-b-2 border-dotted sm:ml-1" />
-            <AppModalTopFirstPart />
-            <div className="ml-6 mt-[13.5rem] border-b-2 border-dotted sm:mt-[15.5rem] sm:ml-1 md:mt-[16.5rem] xl:mt-[19.5rem] " />
-          </ContextPart>
-        </motion.div>
-      )}
-    </AnimatePresence>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1}
+                    strokeOpacity="0.5"
+                    fill="#7a8494"
+                    stroke="black"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <AppModalNaming title={title} />
+              <div className="mt-5 ml-6 border-b-2 border-dotted sm:ml-1" />
+              {more === false ? (
+                <>
+                  <AppModalTopFirstPart />
+                  <div className="ml-6 mt-[13.5rem] border-b-2 border-dotted sm:mt-[15.5rem] sm:ml-1 md:mt-[18rem] lg:mt-[18rem] xl:mt-[17.5rem] " />
+                  <DescriotPart className="ml-5 mt-3 inline-block h-[10rem] w-[30rem] pr-3 sm:w-11/12 ">
+                    <p className="line-clamp-3 ">
+                      {description}
+                      <div
+                        onClick={onMoreClick}
+                        className="absolute bottom-[9.3rem] left-[27.5rem] cursor-pointer text-xs transition-opacity ease-out hover:text-indigo-700 sm:bottom-[7.4rem] sm:left-[32rem] md:bottom-[4.9rem] md:left-[37rem] xl:bottom-[5.3rem]"
+                      >
+                        더 보기
+                      </div>
+                    </p>
+                  </DescriotPart>
+                </>
+              ) : (
+                <MoreDescriotPart>
+                  <motion.p
+                    initial={{
+                      opacity: 0,
+                    }}
+                    animate={{
+                      y: [100, -40, 0],
+                      opacity: [0, 0, 1, 1],
+                    }}
+                    exit={{
+                      y: [0, 100],
+                    }}
+                    className="absolute ml-5 mt-5 w-[29rem] "
+                  >
+                    {description}
+                  </motion.p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    onClick={() => setMore(false)}
+                    className="absolute bottom-[5.5rem] ml-[28rem] h-6 w-6 cursor-pointer text-xs transition-opacity ease-out hover:text-indigo-700 "
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </MoreDescriotPart>
+              )}
+            </ContextPart>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    )
   );
 }
 
 const ContextPart = styled(motion.div)``;
+const DescriotPart = styled(motion.div)``;
+const MoreDescriotPart = styled(motion.div)``;
--- pages/cloneCoding/koreanLanguage/index.tsx
+++ pages/cloneCoding/koreanLanguage/index.tsx
@@ -1,26 +1,23 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { animate, AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import banana1 from "./image/banana1.png";
+import banana2 from "./image/banana2.png";
+import { useState } from "react";
 
 const KoreanLanguage: NextPage = () => {
+  const [isHover, setIsHover] = useState(false);
   return (
     <div>
-      <div className="h-[100vh] w-full bg-gradient-to-r from-[#FFFF00] via-[#FFEA11] to-[#FFFF00] ">
-        <div className="grid h-full w-full grid-flow-col grid-cols-3">
-          <div className="grid grid-flow-row grid-rows-6 items-center justify-center">
-            <div className="h-1/6 w-full">a</div>
-            <div className="h-1/6 w-full">b</div>
-            <div className="h-1/6 w-full">c</div>
-            <div className="h-1/6 w-full">d</div>
-            <div className="h-1/6 w-full">e</div>
-            <div className="h-1/6 w-full">f</div>
-          </div>
-          <div className="grid grid-flow-row grid-rows-3 items-center justify-center">
-            <div className="mb-10 ml-7 w-full justify-center text-2xl font-bold ">
+      <div className="h-[105vh] w-full overflow-y-hidden bg-gradient-to-r from-[#FFFF00] via-[#FFEA11] to-[#FFFF00] ">
+        <AnimatePresence>
+          <div className="flex flex-col items-center justify-center">
+            <div className="mt-20 flex h-40 w-full items-center justify-center text-2xl font-bold">
               Korean Quiz Theme 1
             </div>
-            <div className="ml-10 mt-24 h-full w-full cursor-pointer items-center justify-center">
-              <Link href="/cloneCoding/koreanLanguage/wordCorrect/ ">
+            <div className="flex h-full w-[80%] cursor-pointer items-center justify-center">
+              <Link href={"/cloneCoding"}>
                 <motion.div
                   variants={startBox}
                   initial="entry"
@@ -29,23 +26,73 @@ const KoreanLanguage: NextPage = () => {
                     boxShadow: "0px 0px 8px white",
                     scale: 1.25,
                   }}
-                  className="flex h-24 w-52 flex-col items-center rounded-full bg-[#C3FF99] shadow-xl hover:bg-[#00ffd1] "
+                  className="ml-20 flex h-[5rem] w-[15rem] flex-col items-center rounded-full bg-[#C3FF99] shadow-xl hover:bg-[#00ffd1] "
                 >
-                  <h4 className="mt-9 flex items-center justify-center text-xl font-bold text-teal-700 ">
-                    START
+                  <h4 className="mt-7 flex items-center justify-center text-xl font-bold text-teal-700 ">
+                    To Home
                   </h4>
+                </motion.div>
+              </Link>
+              <Link href="/cloneCoding/koreanLanguage/wordCorrect/ ">
+                <motion.div className="relative mt-10 h-[25rem] w-[20rem]">
+                  <motion.div
+                    onMouseEnter={() => setIsHover(true)}
+                    onMouseLeave={() => setIsHover(false)}
+                    variants={bananaBox}
+                  >
+                    {isHover === false ? (
+                      <Image
+                        src={banana2}
+                        alt="banana2"
+                        layout="fill"
+                        placeholder="blur"
+                        className=" object-fill "
+                      />
+                    ) : (
+                      <Image
+                        src={banana1}
+                        alt="banana1"
+                        layout="fill"
+                        placeholder="blur"
+                        className=" object-contain "
+                      />
+                    )}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={0.3}
+                      stroke="currentColor"
+                      className="absolute left-[12rem] z-0 ml-0 -mt-16 h-64 w-64 "
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                      />
+                      {isHover === false ? (
+                        <>
+                          <text x="7" y="12" fontSize="3">
+                            Hover
+                          </text>
+                          <text x="7" y="16" fontSize="3">
+                            Me
+                          </text>
+                        </>
+                      ) : (
+                        <>
+                          <text x="7" y="13" fontSize="3">
+                            START
+                          </text>
+                        </>
+                      )}
+                    </svg>
+                  </motion.div>
                 </motion.div>
               </Link>
             </div>
           </div>
-          <div className="grid grid-flow-row grid-rows-5 items-center justify-center">
-            <div className="h-1/6 w-full">1</div>
-            <div className="h-1/6 w-full">2</div>
-            <div className="h-1/6 w-full">3</div>
-            <div className="h-1/6 w-full">4</div>
-            <div className="h-1/6 w-full">5</div>
-          </div>
-        </div>
+        </AnimatePresence>
       </div>
     </div>
   );
@@ -66,20 +113,19 @@ const startBox = {
     },
   },
 };
+const bananaBox = {
+  animate: {
+    scale: [1, 1.5, 1.3, 1.1, 1],
+    rotate: [0, 0, 180, 180, 0],
+    // borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      times: [0, 0.2, 0.5, 0.8, 1],
+      repeat: Infinity,
+      repeatDelay: 1,
+    },
+  },
+};
 
 export default KoreanLanguage;
-// animate={{
-//   scale: [1, 2, 2, 1, 1],
-//   rotate: [0, 0, 180, 180, 0],
-//   borderRadius: ["0%", "0%", "50%", "50%", "0%"]
-// }}
-// transition={{
-//   duration: 2,
-//   ease: "easeInOut",
-//   times: [0, 0.2, 0.5, 0.8, 1],
-//   repeat: Infinity,
-//   repeatDelay: 1
-// }}
-// <div className="h-[20vh] w-full bg-[#FFDE00] " />
-// <div className="h-[20vh] w-full bg-[#FCE700] " />
-// <div className="h-[20vh] w-full bg-[#FBDF07] " />
